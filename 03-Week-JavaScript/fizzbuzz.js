746bd86c930f9cd571becdd2fcd1b5f72dd86547
natescode <nate@natescode.com>
2024-01-17 19:27:21 -0600

    WIP

--- 03-Week-JavaScript/fizzbuzz.js
+++ 03-Week-JavaScript/fizzbuzz.js
@@ -12,14 +12,44 @@ function fizzbuzz1(n) {
   }
 }
 
+/*
+I like this style of coding better because we only call `console.log` once. This is similar to the
+`single return` style of coding.
+*/
 function fizzbuzz2(n) {
   for (let i = 1; i <= n; ++i) {
-    let message = i;
-    let fizz = i % 5 === 0 ? "fizz" : "";
-    let buzz = i % 3 === 0 ? "buzz" : "";
-    // let message = fizz !== "" || buzz !== "" ? fizz + buzz : i;
+    let message = "";
+    i % 5 === 0 ? (message += "fizz") : "";
+    i % 3 === 0 ? (message += "buzz") : "";
+    message = message === "" ? i : message;
     console.log(message);
   }
 }
 
 fizzbuzz2(19);
+
+// Let's break this code into pieces and make it even more flexible (generic).
+function fizzbuzzMessage(number) {
+  let message = "";
+  i % 5 === 0 ? (message += "fizz") : "";
+  i % 3 === 0 ? (message += "buzz") : "";
+  return message === "" ? i : message;
+}
+
+// All functions that we call in this function are passed in as parameters
+// this is called Dependency Injection
+// this gives us a lot for flexibility to test, and use the function the way we want
+function fizzbuzz(from, to, getMessageFunc, output) {
+  for (let i = from; i <= to; ++i) {
+    let message = getMessageFunc;
+    output(message);
+  }
+}
+
+fizzbuzz(1, 19, fizzbuzzMessage, (s) => console.log(s));
+
+// let's test this
+let output = [];
+fizzbuzz(1, 19, fizzbuzzMessage, (s) => output.push(s));
+// instead of console.log we can make it push the output to our array
+// this allows us to inspect it and
--- 04-Week_Web-APIs/04-02-notes.md
+++ 04-Week_Web-APIs/04-02-notes.md
@@ -1,1 +1,28 @@
 # JS events
+
+## Forms
+
+Let's define a form. We add some validation to makesure we include the email and password.
+HTML has input types for email and password. `type="email"` requires and `@` somewhere and password
+will show `***` for our password for security.
+
+```html
+<form id="myForm">
+  <input type="email" placeholder="email" required />
+  <input type="password" minlength="8" required />
+  <button type="submit">Submit</button>
+</form>
+```
+
+let's save all the form values with their give names to localStorage.
+
+```javascript
+const form = document.querySelector("#myForm");
+form.addEventListener("submit", function (event) {
+  let formData = {
+    email: form.email.value,
+    password: form.password.value,
+  };
+  localStorage.set("formData", JSON.stringify(formData));
+});
+```
